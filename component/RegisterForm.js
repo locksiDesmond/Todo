723ff7ac1557ef8ae5f210dfd8d9f18ac7296ea8
LocksiDesmond <Des.olanrewaju@gmail.com>
2020-08-05 11:33:36 +0100

    Add Sign up Page

---
+++ component/RegisterForm.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import styles from "../styles/Form.module.css";
+export default function RegisterForm() {
+  const { register, errors, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    const { email, password, name } = data;
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+      <div className={styles.formGroup}>
+        <input
+          name="name"
+          className={styles.input}
+          placeholder="Username"
+          ref={register({ required: true })}
+        />
+        <p className={styles.error}>{errors.name && "Username is required"}</p>
+      </div>
+      <div className={styles.formGroup}>
+        <input
+          name="email"
+          placeholder="Email"
+          type="email"
+          className={styles.input}
+          ref={register({ required: true })}
+        />
+        <p className={styles.error}>{errors.email && "Email is required"}</p>
+      </div>
+      <div className={styles.formGroup}>
+        <input
+          name="password"
+          placeholder="Password"
+          type="password"
+          className={styles.input}
+          ref={register({ required: true })}
+        />
+        <p className={styles.error}>
+          {errors.password && "Password is required"}
+        </p>
+      </div>
+      <div className="mt2 flex">
+        <input
+          type="submit"
+          className={`${styles.button} ${styles.buttonCurve}`}
+        />
+      </div>
+    </form>
+  );
+}
