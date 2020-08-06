d3d7189ea7e65048c826a94c3e24f66607cd26ed
LocksiDesmond <Des.olanrewaju@gmail.com>
2020-08-06 10:48:44 +0100

    implement home page and use Main layout to wrap and also ListCard component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,25 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-
+import Main from "./../layout/Main";
+import styles from "../styles/Card.module.css";
+import formStyles from "../styles/Form.module.css";
+import ListCard from "./../component/ListCard";
 export default function Home() {
-  return <div>Rubbish</div>;
+  return (
+    <Main createList>
+      <div className="flex flex--space-between mb--1">
+        <p className="title"> List</p>
+        <button
+          className={`${formStyles.button} button--box-shadow width--auto`}
+        >
+          Create list
+        </button>
+      </div>
+      <div className={styles.cards}>
+        <ListCard title="All" noOfTask="3" />
+        <ListCard title="All" noOfTask="3" />
+        <ListCard />
+        <ListCard />
+        <ListCard />
+      </div>
+    </Main>
+  );
 }
